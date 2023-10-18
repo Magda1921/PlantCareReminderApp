b9acc1600e57e5c36517bfff109e811d7bb7727b
magda <mag.szymura@gmail.com>
2023-10-18 18:06:38 +0200

    create spring projects structure and repo

---
+++ src/main/java/com/example/plantcarereminderapp/PlantCareReminderAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.plantcarereminderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantCareReminderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantCareReminderAppApplication.class, args);
+    }
+
+}
