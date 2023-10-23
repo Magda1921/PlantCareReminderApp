402dc97fe01615559935a3db514db4aef6157c93
magda <mag.szymura@gmail.com>
2023-10-23 12:30:15 +0200

    #3 fixes after code review

--- src/main/java/com/example/plantcarereminderapp/PlantCareReminderAppApplication.java
+++ src/main/java/com/example/plantcarereminderapp/PlantCareReminderAppApplication.java
@@ -1,13 +1,7 @@
 package com.example.plantcarereminderapp;
-
-import com.example.plantcarereminderapp.model.Account;
-import com.example.plantcarereminderapp.model.AccountPlant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @SpringBootApplication
 public class PlantCareReminderAppApplication {
